659fc46aae827a97683d484a954a7c3940a8d644
Nolan Kovacik <noltron000@gmail.com>
2019-01-29 09:54:59 -0800

    👻 invisible categories ooooooo~

--- src/App.js
+++ src/App.js
@@ -4,30 +4,47 @@ import './App.css';
 
 class App extends Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentCat: null
+		}
+	}
+
 	getCategories() {
 		return categories.map((cat) =>
 			<span key={cat}>
-				<button>
+				<button onClick={() => {
+					this.setState({ currentCat: cat })
+				}}>
 					{cat}
 				</button>
-			</span>
+			</span >
 		)
 	}
 
 	getInventory() {
-		return inventory.map((item, index, array) =>
-			<div>
+		return inventory.filter((item) => {
+			const output = item.category === this.state.currentCat
+			return output
+		}).map((item, index, array) =>
+			<div key={item.name}>
 				<h3>{item.name}</h3>
-				<p>${item.price}</p>
-			</div >
+				<small>${item.price}</small>
+				<p>{item.description}</p>
+			</div>
 		)
+	}
+
+	filterCat(cat) {
+		this.state.currentCat = cat;
 	}
 
 	render() {
 		return (
 			<div className="App">
 				<h1>Show products here</h1>
-
+				<p>Current category is: {this.state.currentCat}</p>
 				<div>
 					{/* Products listed here */
 						this.getCategories()
