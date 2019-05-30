0844efb69a7b919be6d6d5e7076b1a08cd28749f
Nolan Kovacik <noltron000@gmail.com>
2019-05-30 11:42:44 -0700

    late commit

--- src/product-card.js
+++ src/product-card.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class ProductCard extends Component {
 	render() {
 		return (
-			<span>
+			<div>
 				<div key={this.props.name}>
 					<h3>{this.props.name}</h3>
 					<small>${this.props.price}</small>
 					<p>{this.props.description}</p>
 				</div>
-			</span>
+			</div>
 		)
 	}
 }
